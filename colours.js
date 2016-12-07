0decbca03e6cf2fa846a4d3ba6072bbd164e6170
nickboon <boonnick@hotmail.com>
2016-12-07 00:46:22 +0000

    Fixed default alpha for colours.js and set black as default colour.

--- colours.js
+++ colours.js
@@ -1,17 +1,18 @@
 (function (app) {
-	function toRgb (colour, alpha) {
-		var defaultAlpha = 0.5,
-			r,
-			g,
-			b,
-			a = a || defaultAlpha;
-		
-		colour = window.parseInt(colour.slice(1), 16);
+	var defaultAlpha = 0.5,
+		defaultColour = '#000000'
+	
+	function toRgb(colour, alpha) {
+		var	r, g, b, 
+			a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha) || defaultAlpha,
+			c = colour || defaultColour;
+					
+		c = window.parseInt(c.slice(1), 16);
 		//parse hex values
-		r = colour >> 16 & 0xff;
-		g = colour >> 8 & 0xff;
-		b = colour & 0xff;
-		a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+		r = c >> 16 & 0xff;
+		g = c >> 8 & 0xff;
+		b = c & 0xff;
+
 		//only use 'rgba' if needed
 		if (a === 1) {
 			return "rgb("+ r +","+ g +","+ b +")";
@@ -23,6 +24,8 @@
 	// create and return API for this module
 	app.createColourObject = function () {		
 		return {
+			defaultAlpha: defaultAlpha,
+			defaultColour: defaultColour,
 			toRgb: toRgb
 		};
 	};
