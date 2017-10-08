c133626f0f203c7b7af2b3cabcf4ba8f5830bab1
nickboon <boonnick@hotmail.com>
2017-10-09 00:15:54 +0100

    Moved style.css to test folder, renamed copy(point) to copyOf(point).

--- tests/testCubeTransformationsApp.js
+++ tests/testCubeTransformationsApp.js
@@ -1,65 +1,63 @@
 /* required diagrams, points, primitives, shapes, solids, cubeTransformations */
-(function (app) {		
-	function createLabelsForCubeVertices(points, primitives, shapes) {
-		var labelSolids = [],
-		i,
-		copy = app.createPointsObject().copy;
-			
-		for (i = points.length - 1; i >= 0; i -= 1)
-		{
-			primitive = (
-				shapes.createLabel('vertex no.' + i, 
-					copy(points[i]),
-					'#bb2222',
-					.8,
-					null,
-					true
-				)
-			);
-			labelSolids.push(primitives.toSolid(primitive))
-		}
-		
-		return labelSolids;
-	}	
+(function(app) {
+    function createLabelsForCubeVertices(points, primitives, shapes) {
+        var labelSolids = [],
+            i,
+            copyOf = app.createPointsObject().copyOf;
 
-	function createSolids(perspective) {
-		var drawing = app.createDrawingObject(perspective),
-			primitives = app.createPrimitivesObject(drawing),
-			shapes = app.createShapesObject(drawing),
-			solids = app.createSolidsObject(primitives),
-			cubeTransformations = app.createCubeTransformationsObject(200),
-			cubePoints = cubeTransformations.getPoints(),
-			cube = solids.createHexahedron(cubePoints);
+        for (i = points.length - 1; i >= 0; i -= 1) {
+            primitive = (
+                shapes.createLabel('vertex no.' + i,
+                    copyOf(points[i]),
+                    '#bb2222',
+                    .8,
+                    null,
+                    true
+                )
+            );
+            labelSolids.push(primitives.toSolid(primitive))
+        }
 
-			cubeTransformations.rotateCubeAboutY(Math.PI / 4);
-			cubeTransformations.tiltCubeAboutX();
-			cubeTransformations.rotateCubeAlongEdgeAboutX();
-			cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
-			cubeTransformations.rotateCubeAlongEdgeAboutX();
-			
-			cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
-			cubeTransformations.tiltBackCubeAboutX();
-			cubeTransformations.rotateCubeAboutY(Math.PI / 2);
-			cubeTransformations.tiltCubeAboutX();
-			
-			cubeTransformations.rotateCubeAlongEdgeAboutX();
-			cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
-			cubeTransformations.rotateCubeAlongEdgeAboutX();
-			//cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
-		
-		return [cube].concat(
-			createLabelsForCubeVertices(cube.points, primitives, shapes)		
-		);
-	}
-	
-	app.run = function () {
-		var 
-			diagram = app.createDefaultFullScreenDiagram(),
-			perspective = diagram.perspective, 
-			
-			solids = createSolids(perspective);
-			
-		 diagram.stage.setSolids(solids);
-	}
+        return labelSolids;
+    }
+
+    function createSolids(perspective) {
+        var drawing = app.createDrawingObject(perspective),
+            primitives = app.createPrimitivesObject(drawing),
+            shapes = app.createShapesObject(drawing),
+            solids = app.createSolidsObject(primitives),
+            cubeTransformations = app.createCubeTransformationsObject(200),
+            cubePoints = cubeTransformations.getPoints(),
+            cube = solids.createHexahedron(cubePoints);
+
+        cubeTransformations.rotateCubeAboutY(Math.PI / 4);
+        cubeTransformations.tiltCubeAboutX();
+        cubeTransformations.rotateCubeAlongEdgeAboutX();
+        cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
+        cubeTransformations.rotateCubeAlongEdgeAboutX();
+
+        cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
+        cubeTransformations.tiltBackCubeAboutX();
+        cubeTransformations.rotateCubeAboutY(Math.PI / 2);
+        cubeTransformations.tiltCubeAboutX();
+
+        cubeTransformations.rotateCubeAlongEdgeAboutX();
+        cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
+        cubeTransformations.rotateCubeAlongEdgeAboutX();
+        //cubeTransformations.rotateCubeAlongFaceDiagonalAboutX();
+
+        return [cube].concat(
+            createLabelsForCubeVertices(cube.points, primitives, shapes)
+        );
+    }
+
+    app.run = function() {
+        var
+            diagram = app.createDefaultFullScreenDiagram(),
+            perspective = diagram.perspective,
+
+            solids = createSolids(perspective);
+
+        diagram.stage.setSolids(solids);
+    }
 })(window.DIAGRAM_APP || (window.DIAGRAM_APP = {}));
-
--- tests/testKeyboardDrivenTransformationsApp.js
+++ tests/testKeyboardDrivenTransformationsApp.js
@@ -1,69 +1,67 @@
 /* requires test, diagrams, points, primitives, shapes, solids, cubeTransformations */
-(function (app) {				
-	function trimAngleUnitTests() {
-		var	test = app.createTestObject(
-				'Given a transformationObject with numberOfRotationalIncrements 5, test return values for the trimAngle method.',
-				app.createTransformationObject(null, 5).trimAngle
-			);
-			
-		test.assertInputExpectedOutput([8], 3);			
-		test.assertInputExpectedOutput([-2], 3);			
-		test.assertInputExpectedOutput([-21], 4);			
-		test.assertInputExpectedOutput([-20], 0);			
-		test.assertInputExpectedOutput([0], 0);			
-		test.assertInputExpectedOutput([5], 0);			
-	}
-				
-	function createLabelsForCubeVertices(points, primitives, shapes) {
-		var labelSolids = [],
-		i,
-		copy = app.createPointsObject().copy;
-			
-		for (i = points.length - 1; i >= 0; i -= 1)
-		{
-			primitive = (
-				shapes.createLabel('vertex no.' + i, 
-					copy(points[i]),
-					'#bb2222',
-					.8,
-					null,
-					true
-				)
-			);
-			labelSolids.push(primitives.toSolid(primitive))
-		}
-		
-		return labelSolids;
-	}	
+(function(app) {
+    function trimAngleUnitTests() {
+        var test = app.createTestObject(
+            'Given a transformationObject with numberOfRotationalIncrements 5, test return values for the trimAngle method.',
+            app.createTransformationObject(null, 5).trimAngle
+        );
 
-	function createSolids(perspective) {
-		var drawing = app.createDrawingObject(perspective),
-			primitives = app.createPrimitivesObject(drawing),
-			shapes = app.createShapesObject(drawing),
-			solids = app.createSolidsObject(primitives),
-			cube = solids.createHexahedron();
-				
-		return [cube].concat(
-			createLabelsForCubeVertices(cube.points, primitives, shapes)		
-		);
-	}
-	
-	app.run = function () {
-		var diagram = app.createDefaultFullScreenDiagram(),
-			stage = diagram.stage,
-			perspective = diagram.perspective, 			
-			solids = createSolids(perspective),
-			transformation = app.createTransformationObject(),
-			transformer = transformation.createKeyboardDrivenTransformer(solids);
-							
-		trimAngleUnitTests();
+        test.assertInputExpectedOutput([8], 3);
+        test.assertInputExpectedOutput([-2], 3);
+        test.assertInputExpectedOutput([-21], 4);
+        test.assertInputExpectedOutput([-20], 0);
+        test.assertInputExpectedOutput([0], 0);
+        test.assertInputExpectedOutput([5], 0);
+    }
 
-		solids[0].points.forEach(function (point) {					
-			transformation.rotatePointAboutY(point, 45);					
-		}); 
+    function createLabelsForCubeVertices(points, primitives, shapes) {
+        var labelSolids = [],
+            i,
+            copyOf = app.createPointsObject().copyOf;
 
-		stage.setSolids(solids);
-		stage.setTransformers([transformer]);
-	}
+        for (i = points.length - 1; i >= 0; i -= 1) {
+            primitive = (
+                shapes.createLabel('vertex no.' + i,
+                    copyOf(points[i]),
+                    '#bb2222',
+                    .8,
+                    null,
+                    true
+                )
+            );
+            labelSolids.push(primitives.toSolid(primitive))
+        }
+
+        return labelSolids;
+    }
+
+    function createSolids(perspective) {
+        var drawing = app.createDrawingObject(perspective),
+            primitives = app.createPrimitivesObject(drawing),
+            shapes = app.createShapesObject(drawing),
+            solids = app.createSolidsObject(primitives),
+            cube = solids.createHexahedron();
+
+        return [cube].concat(
+            createLabelsForCubeVertices(cube.points, primitives, shapes)
+        );
+    }
+
+    app.run = function() {
+        var diagram = app.createDefaultFullScreenDiagram(),
+            stage = diagram.stage,
+            perspective = diagram.perspective,
+            solids = createSolids(perspective),
+            transformation = app.createTransformationObject(),
+            transformer = transformation.createKeyboardDrivenTransformer(solids);
+
+        trimAngleUnitTests();
+
+        solids[0].points.forEach(function(point) {
+            transformation.rotatePointAboutY(point, 45);
+        });
+
+        stage.setSolids(solids);
+        stage.setTransformers([transformer]);
+    }
 })(window.DIAGRAM_APP || (window.DIAGRAM_APP = {}));
-
